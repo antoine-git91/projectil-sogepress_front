be88b136996e29c63c34d996841fc5ca71c4ead0
louchard <louchardantoine@gmail.com>
2021-08-31 15:30:47 +0200

    debut mise en place

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Header from "./components/Header";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
