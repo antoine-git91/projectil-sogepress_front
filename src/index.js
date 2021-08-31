dc5e145412de6a304229ef4f7964a521b2d7238e
louchard <louchardantoine@gmail.com>
2021-08-31 16:42:26 +0200

    Mise en place projet - début de creation de route

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from "./components/Header";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from "./pages/Home";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
+      <Router>
+         <Header />
+          <Route exact path="/">
+              <Home />
+          </Route>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
