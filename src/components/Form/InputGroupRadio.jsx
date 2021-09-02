ca756b972418776b7dfd5e555f73081ebc25e62e
Louison Dupont <dupontlouison@gmail.com>
2021-09-02 12:27:31 +0200

    Création du bouton select

--- src/components/Form/InputGroupRadio.jsx
+++ src/components/Form/InputGroupRadio.jsx
@@ -25,7 +25,10 @@ class InputGroupRadio extends Component {
         <div onChange={this.onChangeValue}>
 
           {data.map((radio, i)=><InputRadio labeltext={radio.label} idRadio={radio.id} valueRadio={radio.value} nameRadio={name} checked={i + 1 ===selected} />)}
-          
+
+          {/* <InputRadio labeltext="Choix n°1" idRadio="id1" nameRadio="groupe1" valueRadio="value1" />
+          <InputRadio labeltext="Choix n°2" idRadio="id2" nameRadio="groupe1" valueRadio="value2" />
+          <InputRadio labeltext="Choix n°3" idRadio="id3" nameRadio="groupe1" valueRadio="value3" /> */}
         </div>
       );
     }
--- src/pages/front/Home/index.jsx
+++ src/pages/front/Home/index.jsx
@@ -12,7 +12,7 @@ const Home = () => {
             <h1>Dashboard</h1>
             <InputGroupRadio selected={3} name="groupe1" data={[{"id": "id1", "label": "Mon label 1", "value": "value1"},{"id": "id2", "label": "Mon label 2", "value": "value2"},{"id": "id3", "label": "Mon label 3", "value": "value3"},{"id": "id4", "label": "Mon label 4", "value": "value4"}]}/>
             <InputText labeltext="Coucou" />
-            <InputSelect />
+            <InputSelect data={[{"id" : "id1", "value" : "Site internet"}, {"id" : "id2", "value" : "Brochure"}, {"id" : "id3", "value" : "Flyer"} ]} />
             <Link to="/creation_client" >Créer un client</Link>
         </MainContainer>
     )
